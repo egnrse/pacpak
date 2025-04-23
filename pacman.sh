a46a2dd9f346d5d63c4573c71431bda57d47c287
Elia Nitsche <nitscheelia@gmail.com>
2025-04-23 02:08:59 +0200

    messing around

--- pacman.sh
+++ pacman.sh
@@ -2,22 +2,36 @@
 # shallow flatpak integration into pacman (proof of concept)
 # (by egnrse)
 
+normal="\e[0m"
+bold="\e[1m"
+green="\e[32m"
+
 args=$@
 
 # execute the command normally
-pacman ${args}
+#pacman ${args}	#dev
 # TODO wrap output/error
 
 program=$2	# file or program
 # TODO search if program is not a exact match (for some args)
 
+# does not work because of different branches (that might be installed at the same time)
+flatpakList=$(flatpak list --columns=application)
+echo $flatpakList
+
+
 case "$1" in
 	-Q)
-		fullList=$(flatpak list --app --columns=application,version,name | awk '{print "\033[1m"$3"\033[0m", "(" $1 ")", "\033[32m"$2"\033[0m"}')
 		if [ -z "${program}" ]; then
-			echo ${fullList}
+			for appid in ${flatpakList}; do
+				# TODO deal with branches
+				flatpakBr=$(flatpak list --columns=application,branch | grep ${appid})
+				#echo ${flatpakBr}
+				#version=$(flatpak info ${appid} | grep Version | awk '{print $2}')
+				#echo -e "${bold}${appid} ${green}${version}${normal}"
+			done
 		else
-			echo ${fullList} | grep ${program}
+			echo ${appid}
 		fi
 		;;
 	-Q*) ;;
@@ -26,3 +40,5 @@ case "$1" in
 	-F*)	;;
 esac
 #flatpak info 
+
+#fullList=$(flatpak list --app --columns=application,version,name | awk '{print "\033[1m"$3"\033[0m", "(" $1 ")", "\033[32m"$2"\033[0m"}')
